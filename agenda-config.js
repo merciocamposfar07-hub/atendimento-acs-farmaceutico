a51681777cc4f3a2b2c249098b111667950c33ae
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-07-25 17:53:00 -0300

    Conectar mural de avisos ao Google Apps Script

--- agenda-config.js
+++ agenda-config.js
@@ -1,11 +1,8 @@
 /*
  * Serviços externos gratuitos usados pelo portal.
- *
- * A agenda odontológica já está ativa no Google Apps Script.
- * O mural de avisos ficará oculto enquanto POSTO_MATIAS_AVISOS_API_URL estiver vazio.
  */
 window.DENTAL_AGENDA_API_URL = 'https://script.google.com/macros/s/AKfycbzB8HKs_sawD2X8K9O3hGjgCge3gao5S9FjajcqYxyO8e_0WTkrsoqjtBhC4kFhAFTl/exec';
-window.POSTO_MATIAS_AVISOS_API_URL = '';
+window.POSTO_MATIAS_AVISOS_API_URL = 'https://script.google.com/macros/s/AKfycby-90dbbC-aMYEmSXxGUef6zshd1SNI85duivD18HJP6sFnkOmAZ9lYda2Cs7mZddpBcw/exec';
 
 (function () {
   'use strict';
