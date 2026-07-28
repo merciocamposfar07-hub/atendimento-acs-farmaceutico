ed2f1fbedc733f34204a97bb2f03fba2a5c00e6c
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-07-28 02:08:06 -0300

    Prepara configuração isolada de notificações

---
+++ notificacoes-config.js
@@ -0,0 +1,6 @@
+window.PORTAL_TACS_NOTIFICACOES = Object.freeze({
+  provider: 'onesignal',
+  appId: '',
+  serviceWorkerPath: 'OneSignalSDKWorker.js',
+  serviceWorkerParam: { scope: '/atendimento-acs-farmaceutico/' }
+});
