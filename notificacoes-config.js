3faab38da11751dece82e11c3481433634306d3f
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-07-28 16:35:19 -0300

    Corrige caminho do service worker no GitHub Pages

--- notificacoes-config.js
+++ notificacoes-config.js
@@ -1,6 +1,6 @@
 window.PORTAL_TACS_NOTIFICACOES = Object.freeze({
   provider: 'onesignal',
   appId: 'e2294b98-c72b-4f8c-a055-de28979676dc',
-  serviceWorkerPath: 'push/OneSignalSDKWorker.js',
+  serviceWorkerPath: 'atendimento-acs-farmaceutico/push/OneSignalSDKWorker.js',
   serviceWorkerParam: { scope: '/atendimento-acs-farmaceutico/push/' }
 });
