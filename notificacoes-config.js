d1d38ce8f0103a3e66889e9eae43a896846407a4
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-07-31 00:00:08 -0300

    Remove rotina antiga notificacoes-config.js

--- notificacoes-config.js
+++
@@ -1,7 +0,0 @@
-window.PORTAL_TACS_NOTIFICACOES = Object.freeze({
-  provider: 'onesignal',
-  appId: 'e2294b98-c72b-4f8c-a055-de28979676dc',
-  safariWebId: 'web.onesignal.auto.4bead971-106d-461b-853f-83aecbd62d40',
-  serviceWorkerPath: 'atendimento-acs-farmaceutico/push/OneSignalSDKWorker.js',
-  serviceWorkerParam: { scope: '/atendimento-acs-farmaceutico/push/' }
-});
