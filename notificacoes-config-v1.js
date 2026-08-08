e5d32c0eb3b5b216b0229e5b2401e0f306228f9a
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-08-08 00:43:35 -0300

    Push V1: adicionar configuração pública isolada do OneSignal

---
+++ notificacoes-config-v1.js
@@ -0,0 +1,12 @@
+(function(){
+'use strict';
+window.PORTAL_TACS_NOTIFICACOES_V1=Object.freeze({
+  provider:'onesignal',
+  version:'1.0.0',
+  appId:'e2294b98-c72b-4f8c-a055-de28979676dc',
+  safariWebId:'web.onesignal.auto.4bead971-106d-461b-853f-83aecbd62d40',
+  serviceWorkerPath:'push/OneSignalSDKWorker.js',
+  serviceWorkerParam:{scope:'/atendimento-acs-farmaceutico/push/'},
+  portalUrl:'https://merciocamposfar07-hub.github.io/atendimento-acs-farmaceutico/'
+});
+}());
