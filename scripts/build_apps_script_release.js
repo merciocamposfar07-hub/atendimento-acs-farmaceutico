fb0e0364a1bd917bad5dfef1b2508502e321c481
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-08-14 00:00:43 -0300

    Incluir restauração de nascimento no pacote Apps Script

--- scripts/build_apps_script_release.js
+++ scripts/build_apps_script_release.js
@@ -48,6 +48,10 @@ const MODULES = [
   {
     source: 'apps-script/ZZZZ_23_CorrecaoNascimentoMaisUmDiaV1.gs',
     marker: 'TACS_CORRECAO_NASCIMENTO_V1'
+  },
+  {
+    source: 'apps-script/ZZZZ_24_RestauracaoNascimentoBackupV1.gs',
+    marker: 'TACS_RESTAURACAO_NASCIMENTO_V1'
   }
 ];
 
