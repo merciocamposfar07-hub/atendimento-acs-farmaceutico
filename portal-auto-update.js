b0913d9865fecba3cb72d72a072adc42052f2bee
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-09-15 19:07:45 -0300

    chore(morador): renovar cache do bloco familiar corrigido

--- portal-auto-update.js
+++ portal-auto-update.js
@@ -305,7 +305,7 @@
     var script=document.createElement('script');
     script.id='portalTacsIdentificacaoFamiliaScriptV1';
     script.async=true;
-    script.src='/atendimento-acs-farmaceutico/portal-identificacao-familia-v1.js?v=20260823-bloco9-v1';
+    script.src='/atendimento-acs-farmaceutico/portal-identificacao-familia-v1.js?v=20260915-familia-direta-v1';
     (document.head||document.documentElement).appendChild(script);
   }
 
