09924e75057e3c3d95c7686b9158d3469f19642d
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-09-15 20:51:17 -0300

    chore(morador): renovar cache do fluxo CPF nascimento PIN

--- portal-auto-update.js
+++ portal-auto-update.js
@@ -305,7 +305,7 @@
     var script=document.createElement('script');
     script.id='portalTacsIdentificacaoFamiliaScriptV1';
     script.async=true;
-    script.src='/atendimento-acs-farmaceutico/portal-identificacao-familia-v1.js?v=20260915-cpf-pin-direto-v5';
+    script.src='/atendimento-acs-farmaceutico/portal-identificacao-familia-v1.js?v=20260915-cpf-nascimento-pin-v6';
     (document.head||document.documentElement).appendChild(script);
   }
 
