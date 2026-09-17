b322fd10512c25bad27750e0989390cfc114fb35
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-09-16 21:10:13 -0300

    chore: renovar cache da abertura imediata de Moradores

--- central-administrativa-tacs.js
+++ central-administrativa-tacs.js
@@ -857,7 +857,7 @@ function ensureTask17MoradoresAssets(callback){
   }
   task16LoadScript('cscModuleCoreTask17','/atendimento-acs-farmaceutico/conecta-module-core-v1.js?v=20260916-cache-persistente-v1',function(){return Boolean(window.ConectaModuleCoreV1)},function(ok){
     if(!ok){finish(false);return}
-    task16LoadScript('cscMoradoresNativeTask17','/atendimento-acs-farmaceutico/conecta-moradores-native-v1.js?v=20260916-isolamento-area-v3',function(){return Boolean(window.ConectaMoradoresNativeV1)},finish);
+    task16LoadScript('cscMoradoresNativeTask17','/atendimento-acs-farmaceutico/conecta-moradores-native-v1.js?v=20260916-moradores-abertura-imediata-v1',function(){return Boolean(window.ConectaMoradoresNativeV1)},finish);
   });
 }
 function showNativeMoradores(title,routeId){
