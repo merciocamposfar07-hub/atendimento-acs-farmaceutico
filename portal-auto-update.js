de478325b70a768dacf1a0edbd724dacec6762ee
merciocamposfar07-hub <merciocamposfar07@icloud.com>
2026-09-18 21:31:00 -0300

    chore(portal): publicar persistência do PIN de teste

--- portal-auto-update.js
+++ portal-auto-update.js
@@ -305,7 +305,7 @@
     var script=document.createElement('script');
     script.id='portalTacsIdentificacaoFamiliaScriptV1';
     script.async=true;
-    script.src='/atendimento-acs-farmaceutico/portal-identificacao-familia-v1.js?v=20260918-pin-notificacao-ordem-v9';
+    script.src='/atendimento-acs-farmaceutico/portal-identificacao-familia-v1.js?v=20260918-pin-teste-persistente-v10';
     (document.head||document.documentElement).appendChild(script);
   }
 
